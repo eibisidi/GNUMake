b33a6682af6300a14f9ac6c08114dd517cc0958f
husy <eibisidi@qq.com>
2024-09-24 23:47:41 -0700

    add makefiles

---
+++ ch10.mak-bash.mk
@@ -0,0 +1,16 @@
+#page 183 
+SHELL := /bin/bash
+.PHONY:x
+
+x:
+	$(MAKE) --no-print-directory --slient --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;\
+	$(MAKE) --no-print-directory --silent --question ch10.make-bash.mk;
+	
